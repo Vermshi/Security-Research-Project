3136ccbbd849719289713315916e2ecee15acaa5
jakobsn <jakob_sn_@hotmail.com>
2018-10-09 15:02:54 +0200

    mvp with passive scans

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 from testclient import testClient
+import time
 
 if __name__ == '__main__':
     testclient = testClient()
-    testclient.setPort("7070")
     testclient.setTarget("http://0.0.0.0:8080/")
-    testclient.zapTest()
+    testclient.runAllTests()
+    print testclient.tests
