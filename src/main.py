05d05368b895031321756858d7e105533f56a6a7
mrCardizzle <shivamv@stud.ntnu.no>
2018-10-09 15:21:27 +0200

    add GUI script

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from testclient import testClient
 import time
 
 if __name__ == '__main__':
-    testclient = testClient()
-    testclient.setTarget("http://0.0.0.0:8080/")
+    testclient = testClient(zapPort="8080")
+    testclient.setTarget("http://0.0.0.0:80/")
     testclient.runAllTests()
     print testclient.tests
